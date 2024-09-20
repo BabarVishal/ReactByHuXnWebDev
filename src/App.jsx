016858507da5ae7d7dd5b2a55b4fb6c999c3e52b
vishal <vishalbabar7351@gmail.com>
2024-09-21 00:11:24 +0530

    what is jsx, components,fragments, expressionsInJsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Add from './componats/Add'
+import Greeding from './componats/Greeding'
 
 function App() {
   return (
     <div>
       <h1>Hello Vishal!</h1>
+      <h2>I reStart my Reat_js With huxn</h2>
+      <h3>Welcome to the react world 🥰</h3>
+      <Add />
+      <Greeding />
     </div>
   )
 }
